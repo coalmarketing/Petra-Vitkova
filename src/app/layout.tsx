eb36abd3b43cc90476da565713eee94b21a98ed0
Ondřej Krejčí <ondrej.krejci@coalsoft.cz>
2025-05-29 15:19:42 +0200

    favicon

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Petra Vitková",
+  description: "Osobní web Petra Vitkové",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: '/favicon/favicon.ico',
+    apple: '/favicon/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/favicon/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/favicon/android-chrome-512x512.png',
+      },
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="cs">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+} 
